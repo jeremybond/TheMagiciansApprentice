d9111bc9ac6eb5f32256cbe60fca01d44d92d087
JeReMy bond <JeReMy bond>
2016-07-25 01:15:48 +0200

    added fireball spell

---
+++ Assets/Scripts/Spells/SpellCollision.cs
@@ -0,0 +1,30 @@
+﻿//Created By: Jeremy Bond
+//Date: 24/07/2016
+
+using UnityEngine;
+
+public class SpellCollision : MonoBehaviour 
+{
+	[SerializeField] private GameObject spell;
+	[SerializeField] private GameObject explosion;
+
+	/// <summary>
+	/// Awake function that activates the spell.
+	/// </summary>
+	protected void Awake()
+	{
+		spell.SetActive(true);
+	}
+	/// <summary>
+	/// OnCollision enter function that triggers when the spell collides with anything.
+	/// </summary>
+	/// <param name="col"></param>
+	protected void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.tag != ConstStrings.PLAYERTAG)
+		{
+			spell.SetActive(false);
+			explosion.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/Spells/SpellMovement.cs
@@ -0,0 +1,15 @@
+﻿//Created By: Jeremy Bond
+//Date: 24/07/2016
+
+using UnityEngine;
+
+public class SpellMovement : MonoBehaviour 
+{
+	/// <summary>
+	/// Update function that moves the spell.
+	/// </summary>
+	protected void Update ()
+	{
+		transform.position = Vector3.Lerp(transform.position, transform.position + (Vector3.up / 20), Time.time);
+	}
+}
