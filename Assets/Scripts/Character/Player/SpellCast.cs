3088de5e17db3e1dea0c3683f15537fb2cc983ed
JeReMy bond <JeReMy bond>
2016-07-26 20:51:35 +0200

    placed spellcast and spellactivation scripts inside player folder in the character folder

---
+++ Assets/Scripts/Character/Player/SpellCast.cs
@@ -0,0 +1,31 @@
+﻿//Created By: Jeremy Bond
+//Date: 26/07/2016
+
+using UnityEngine;
+
+public class SpellCast : MonoBehaviour 
+{
+	private bool canCast = true;
+	/// <summary>
+	/// Update function that triggers the cast spell event when spacebar is pressed.
+	/// </summary>
+	protected void Update () 
+	{
+		if (canCast)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				EventManager.TriggerEvent(GeneralEvents.SPELLCAST);
+				canCast = false;
+				Invoke("EnableCast", .5f);
+			}
+		}
+	}
+	/// <summary>
+	/// Function that enables casting again.
+	/// </summary>
+	private void EnableCast()
+	{
+		canCast = true;
+	}
+}
