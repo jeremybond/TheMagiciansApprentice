466e656d2fa9eec1ecb03014a34f481da9803b1d
JeReMy bond <JeReMy bond>
2016-07-29 16:32:12 +0200

    added pick up

---
+++ Assets/Scripts/PickUps/LifePickUp.cs
@@ -0,0 +1,27 @@
+﻿//Created By: Jeremy Bond
+//Date: 27/07/2016
+
+using UnityEngine;
+
+public class LifePickUp : MonoBehaviour
+{
+	[SerializeField] private int livesGained;
+	[SerializeField] private Collider2D[] colliders;
+
+	/// <summary>
+	/// When colliding with the player, the adjust life event is triggered.
+	/// </summary>
+	/// <param name="col">reference to the collider2d that hit this object.</param>
+	protected void OnTriggerEnter2D (Collider2D col) 
+	{
+		if (col.transform.tag == ConstStrings.PLAYERTAG)
+		{
+			EventManager.TriggerAdjustLifeEvent(livesGained);
+			foreach (Collider2D coll in colliders)
+			{
+				coll.enabled = false;
+			}
+			Destroy (gameObject, 0.1f);
+		}
+	}
+}
