c15fc5834d2de627bca13c9694711fdb4e2358f2
JEREMYLAPTOP\JeReMy <jeremybond2@hotmail.com>
2019-05-12 20:59:45 +0200

    Finished the first house, the stairs work and exit work.

--- Assets/Scripts/House/UpStairs.cs
+++ Assets/Scripts/House/UpStairs.cs
@@ -2,18 +2,33 @@
 // Date: 12/05/2019
 
 using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class UpStairs : MonoBehaviour 
+public class UpStairs : MonoBehaviour
 {
+    [SerializeField] private bool GoingUp;
+    [SerializeField] private GameObject groundFloor;
+    [SerializeField] private GameObject upStairs;
 
-	protected void Awake () 
-	{
-	
-	}
-	
-	protected void Update () 
-	{
-	
-	}
+
+    /// <summary>
+    /// The OnTrigger function is used to check when a player will enter the collider.
+    /// </summary>
+    /// <param name="col"></param>
+    protected void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.gameObject.name == ConstStrings.PLAYERTAG)
+        {
+            if (GoingUp)
+            {
+                groundFloor.SetActive (false);
+                upStairs.SetActive(true);
+            }
+            else
+            {
+                groundFloor.SetActive(true);
+                upStairs.SetActive(false);
+            }
+        }
+    }
 }
