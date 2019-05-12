116058c618d04b92f6e7cb65ffb3a4c033b2d8f3
JEREMYLAPTOP\JeReMy <jeremybond2@hotmail.com>
2019-05-12 20:22:39 +0200

    Implementing the player house design + adding a upstairs script that doens't work yet!

---
+++ Assets/Scripts/House/UpStairs.cs
@@ -0,0 +1,19 @@
+﻿// Created By: Jeremy Bond
+// Date: 12/05/2019
+
+using UnityEngine;
+using System.Collections;
+
+public class UpStairs : MonoBehaviour 
+{
+
+	protected void Awake () 
+	{
+	
+	}
+	
+	protected void Update () 
+	{
+	
+	}
+}
